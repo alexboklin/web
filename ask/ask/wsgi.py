cd96504f02d22e2d9100636e864ec6e6fd557c91
Alex Boklin <alex.boklin@gmail.com>
2016-05-17 12:57:31 +0300

    Update wsgi.py

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -7,7 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+sys.path.append('/home/box/web/ask')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
 
 from django.core.wsgi import get_wsgi_application
