d6af57a22df6ef29a665c3761ab3a5ac505733bf
Alex Boklin <alex.boklin@gmail.com>
2016-05-22 22:36:00 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,3 +5,5 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
+
+sudo /etc/init.d/mysql restart
