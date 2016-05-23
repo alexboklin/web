f759b704f7ff8d2a1078ec2dc4880e0254125644
Alex Boklin <alex.boklin@gmail.com>
2016-05-23 10:33:33 +0300

    Update wsgi.py

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os, sys
-sys.path.append('/home/box/web/ask')
+import os
+
+#, sys
+#sys.path.append('/home/box/web/ask')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
 
