8e0bb395960f404b818bf8897656432b2ce1b956
Alex Boklin <alex.boklin@gmail.com>
2016-05-23 11:09:57 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -59,7 +59,8 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
